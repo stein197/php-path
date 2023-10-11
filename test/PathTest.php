9f28b30dfad1c284714d6fee4cbb2fa18cc92e66
Nail Gafarov <nil20122013@gmail.com>
2023-10-11 21:57:17 +0200

    Added tests for Path::__construct()

--- test/PathTest.php
+++ test/PathTest.php
@@ -1,11 +1,20 @@
 <?php
 namespace Stein197;
 
+use InvalidArgumentException;
 use function describe;
 use function expect;
 use function test;
 
-describe('\\Stein197\\Path::__construct()', function () {})->skip();
+describe('\\Stein197\\Path::__construct()', function () {
+	test('Should throw an exception when the string is empty', function () {
+		expect(fn () => new Path(''))->toThrow(InvalidArgumentException::class, 'Cannot instantiate a path object: the path string is empty');
+	});
+	test('Should instantiate an object when the string is not empty', function () {
+		$p = new Path('public/index.html');
+		expect($p->raw)->toBe('public/index.html');
+	});
+});
 describe('\\Stein197\\Path::__toString()', function () {})->skip();
 describe('\\Stein197\\Path::isAbsolute()', function () {})->skip();
 describe('\\Stein197\\Path::isRelative()', function () {})->skip();
