26ad1dc26c4d52dfb39a6286c2234570e5020b02
Nail Gafarov <nil20122013@gmail.com>
2023-10-12 19:51:07 +0200

    Added tests for Path::getType()

--- test/PathTest.php
+++ test/PathTest.php
@@ -15,12 +15,34 @@ describe('\\Stein197\\Path::__construct()', function () {
 		expect($p->raw)->toBe('public/index.html');
 	});
 });
+
 describe('\\Stein197\\Path::__toString()', function () {})->skip();
 describe('\\Stein197\\Path::isAbsolute()', function () {})->skip();
 describe('\\Stein197\\Path::isRelative()', function () {})->skip();
 describe('\\Stein197\\Path::isRoot()', function () {})->skip();
 describe('\\Stein197\\Path::getParent()', function () {})->skip();
-describe('\\Stein197\\Path::getType()', function () {})->skip();
+
+describe('\\Stein197\\Path::getType()', function () {
+	test('Windows', function () {
+		expect((new Path('C:'))->getType())->toBe(PathType::Windows);
+		expect((new Path('c:'))->getType())->toBe(PathType::Windows);
+		expect((new Path('c:\\'))->getType())->toBe(PathType::Windows);
+		expect((new Path('C:/'))->getType())->toBe(PathType::Windows);
+		expect((new Path('C:\\Windows'))->getType())->toBe(PathType::Windows);
+		expect((new Path('C:/Windows'))->getType())->toBe(PathType::Windows);
+	});
+	test('Unix', function () {
+		expect((new Path('/'))->getType())->toBe(PathType::Unix);
+		expect((new Path('\\'))->getType())->toBe(PathType::Unix);
+		expect((new Path('/usr'))->getType())->toBe(PathType::Unix);
+		expect((new Path('\\usr/bin'))->getType())->toBe(PathType::Unix);
+	});
+	test('null', function () {
+		expect((new Path('filename.txt'))->getType())->toBeNull();
+		expect((new Path('.git'))->getType())->toBeNull();
+	});
+});
+
 describe('\\Stein197\\Path::toAbsolute()', function () {})->skip();
 describe('\\Stein197\\Path::toRelative()', function () {})->skip();
 describe('\\Stein197\\Path::format()', function () {})->skip();
